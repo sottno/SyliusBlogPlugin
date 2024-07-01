815ce7b50ed8681cbd9f1d89ea51aebe1f05416a
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2024-07-01 10:16:12 +0200

    feat(setup): init plugin with plugin maker

---
+++ src/DependencyInjection/MonsieurBizSyliusBlogExtension.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz's  for Sylius.
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusBlogPlugin\DependencyInjection;
+
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * @SuppressWarnings(PHPMD.LongClassName)
+ */
+final class MonsieurBizSyliusBlogExtension extends Extension implements PrependExtensionInterface
+{
+    use PrependDoctrineMigrationsTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public function load(array $config, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAlias(): string
+    {
+        return 'monsieurbiz_blog';
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDoctrineMigrations($container);
+    }
+
+    protected function getMigrationsNamespace(): string
+    {
+        return 'MonsieurBiz\SyliusBlogPlugin\Migrations';
+    }
+
+    protected function getMigrationsDirectory(): string
+    {
+        return '@MonsieurBizSyliusBlogPlugin/Migrations';
+    }
+
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return [
+            'Sylius\Bundle\CoreBundle\Migrations',
+        ];
+    }
+}
